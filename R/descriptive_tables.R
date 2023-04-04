927afe68afc4f7ad6e9402edcd574f56373fe03f
garcia-galea <eduardo.garcia.galea@gmail.com>
2023-04-04 16:36:09 +0200

    First summarise_df version

--- R/descriptive_tables.R
+++ R/descriptive_tables.R
@@ -1,5 +1,4 @@
-#' @importFrom rlang .data
-#' @importFrom rlang :=
+#' @importFrom rlang .data :=
 
 # Helper function to complete a list. Useful to complete a conditions_list.
 # It returns the same list with any empty ("") value replaced by the previous
@@ -24,9 +23,14 @@ complete_list <- function(list_to_complete) {
 
 # Helper function to create a list of the variables to be described. It returns
 # a list where each element is a data.frame with a variable. If required, the
-# variable is filtered according to conditions_list. by_var variables are also
+# variable is filtered according to conditions_list. Grouping variables are also
 # added if required.
-make_var_list <- function(data_frame, conditions_list = NULL, by_var = NULL) {
+make_var_list <- function(data_frame, conditions_list = NULL, grouping_var = NULL) {
+
+  if (!is.null(grouping_var) &
+     (!is.character(grouping_var) | length(grouping_var) != 1)) {
+    stop("grouping_var must be a length 1 character vector")
+  }
 
   data_nested <- tibble::tibble(
     name_var = names(data_frame),
@@ -53,45 +57,135 @@ make_var_list <- function(data_frame, conditions_list = NULL, by_var = NULL) {
 
   purrr::pmap(
     data_nest_cond,
-    function(data_list, rel_index, name_var, condition, by_var) {
+    function(data_list, rel_index, name_var, condition, grouping_var) {
       if (rel_index) {
         data_list |>
           dplyr::filter(eval(str2lang(condition))) |>
-          dplyr::select(tidyselect::any_of(c(name_var, by_var)))
+          dplyr::select(tidyselect::any_of(c(name_var, grouping_var)))
       } else {
         data_list |>
-          dplyr::select(tidyselect::any_of(c(name_var, by_var)))
+          dplyr::select(tidyselect::any_of(c(name_var, grouping_var)))
       }
     },
-    by_var
+    grouping_var
   )
 
 }
 
+# Helper function to create a tibble-like summary for both numeric or date
+# variables.
+summary_tibble <- function(x, num_dec = 3) {
 
-# Helper function to create a tibble-like summary
-summary_tibble <- function(x) {
-  tibble::tibble(
-    `Min.` = min(x, na.rm = TRUE),
-    `1st Qu.` = quantile(x, 0.25, na.rm = TRUE),
-    Median = median(x, na.rm = TRUE),
-    Mean = mean(x, na.rm = TRUE),
-    SD = sd(x, na.rm = TRUE),
-    `3rd. Qu.` = quantile(x, 0.75, na.rm = TRUE),
-    `Max.` = max(x, na.rm = TRUE),
-    "NA" = sum(is.na(x))
-  )
+  if (lubridate::is.Date(x)) {
+
+    tibble::tibble(
+      N = sum(!is.na(x)),
+      `Min.` = min(x, na.rm = TRUE),
+      `1st Qu.` = stats::quantile(x, 0.25, na.rm = TRUE, type = 1),
+      Median = stats::median(x, na.rm = TRUE),
+      Mean = mean(x, na.rm = TRUE) |> round(num_dec),
+      `3rd Qu.` = stats::quantile(x, 0.75, na.rm = TRUE, type = 1),
+      `Max.` = max(x, na.rm = TRUE),
+      `NA` = sum(is.na(x))
+    )
+
+  } else {
+
+    tibble::tibble(
+      N = sum(!is.na(x)),
+      `Min.` = min(x, na.rm = TRUE),
+      `1st Qu.` = stats::quantile(x, 0.25, na.rm = TRUE),
+      Median = stats::median(x, na.rm = TRUE),
+      Mean = mean(x, na.rm = TRUE) |> round(num_dec),
+      SD = stats::sd(x, na.rm = TRUE) |> round(num_dec),
+      `3rd Qu.` = stats::quantile(x, 0.75, na.rm = TRUE),
+      `Max.` = max(x, na.rm = TRUE),
+      `NA` = sum(is.na(x))
+    )
+
+  }
+
 }
 
+# Helper function to summarise continuous variables (including dates). If there
+# is a grouping_var, the summary is performed by each level of grouping_var.
 summarise_continous_var <- function(cont_var,
                                     use_NA = c("no", "ifany", "always"),
                                     num_dec = 3) {
-  "go!"
+
+  use_NA <- rlang::arg_match(use_NA)
+
+  if (ncol(cont_var) == 1) {
+    full_summary <- summary_tibble(cont_var[[1]]) |>
+      dplyr::select(-N)
+    if (use_NA == "no" | (use_NA == "ifany" & full_summary[["NA"]] == 0)) {
+      return(dplyr::select(full_summary, -`NA`))
+    } else {
+       return(full_summary)
+    }
+  }
+
+  if (ncol(cont_var) == 2) {
+    # Grouping var (which is in column 2 of cont_var) is forced as a factor to
+    # have control over the order its levels are shown.
+    cont_var[, 2] <- factor(cont_var[, 2])
+    overall_data <- cont_var[[1]]
+    by_group_data <- tidyr::nest(cont_var, data = 1) |>
+      dplyr::filter(!is.na(.data[[names(cont_var)[2]]]))
+    factor_order <- order(as.numeric(by_group_data[[1]]))
+    missing_data <- cont_var[is.na(cont_var[[2]]), ]
+
+    full_summary <- dplyr::bind_rows(
+      # Overall summary
+      summary_tibble(overall_data, num_dec),
+      # By levels summaries
+      purrr::map_dfr(
+        by_group_data$data, ~summary_tibble(.[[1]]), num_dec
+      )[factor_order, ],
+      # Missing level summary
+      summary_tibble(cont_var[is.na(cont_var[[2]]), ][[1]]) |>
+        suppressWarnings()
+    ) |>
+      dplyr::mutate(
+        "{names(cont_var)[2]}" := c(
+          "Overall", levels(by_group_data[[1]]), "NA"
+        ),
+        .before = "N"
+      )
+
+    missing_levels <- tail(full_summary$N, 1) != 0
+    missing_values <- full_summary$`NA`[1] != 0
+
+  }
+
+  if (
+    use_NA == "no" |
+    (use_NA == "ifany" & !missing_levels & !missing_values)
+  ) {
+    return(
+      full_summary |>
+        dplyr::filter(.data[[colnames(cont_var)[2]]] != "NA") |>
+        dplyr::select(-`NA`)
+    )
+  } else if (use_NA == "always") {
+    return(full_summary)
+  } else {
+    if (!missing_levels) {
+      full_summary <- full_summary |>
+        dplyr::filter(.data[[colnames(cont_var)[2]]] != "NA")
+    }
+    if (!missing_values) {
+      full_summary <- full_summary |>
+        dplyr::select(-`NA`)
+    }
+    return(full_summary)
+  }
+
 }
 
 # Helper function to count and calculate proportions of discrete variables.
-# Previous controls should ensure that this function is not feed with
-# continuous variables.
+# 1D table if no grouping_var is suplied to make_var_list. Otherwise, a 2d table
+# including an overall.
 summarise_discrete_var <- function(disc_var,
                                    use_NA = c("no", "ifany", "always"),
                                    prop_dec = 3) {
@@ -105,7 +199,9 @@ summarise_discrete_var <- function(disc_var,
       n = as.numeric(raw_count),
       prop = round(prop.table(raw_count), prop_dec) |> as.numeric()
     )
-  } else if (ncol(disc_var) == 2) {
+  }
+
+  if (ncol(disc_var) == 2) {
     # 2D count
     raw_table <- table(disc_var, useNA = use_NA)
     raw_n_table <- tibble::as_tibble(raw_table)
@@ -132,9 +228,8 @@ summarise_discrete_var <- function(disc_var,
         names_from = colnames(disc_var[2]),
         values_from = c("n", "prop"),
         names_vary = "slowest"
-      )
-  } else{
-    stop("disc_var has more than 2 columns")
+      ) |>
+      suppressMessages()
   }
 
   # NA to "NA" and NaN to 0
@@ -150,3 +245,34 @@ summarise_discrete_var <- function(disc_var,
     )
 }
 
+# Function to run summarise_ functions for each variable of a data.frame
+summarise_df <- function(data_frame,
+                         grouping_var = NULL,
+                         conditions_list = NULL,
+                         use_NA = c("no", "ifany", "always"),
+                         num_dec = 3, prop_dec = 3) {
+
+  use_NA <- rlang::arg_match(use_NA)
+
+  if (!is.null(conditions_list)) {
+    conditions_list <- complete_list(conditions_list)
+  }
+
+  var_list <- make_var_list(data_frame, conditions_list, grouping_var)
+
+  summaries <- purrr::map(
+    var_list,
+    function(x) {
+      if (is.numeric(x[[1]]) | lubridate::is.Date(x[[1]])) {
+        summarise_continous_var(x, use_NA, num_dec)
+      } else {
+        summarise_discrete_var(x, use_NA, prop_dec)
+      }
+    }
+  )
+
+  names(summaries) <- purrr::map_chr(var_list, ~colnames(.)[1])
+
+  summaries
+
+}
--- sandbox/sandbox.R
+++ sandbox/sandbox.R
@@ -1,35 +1,60 @@
 data_frame <- medicaldata::smartpill |>
   janitor::clean_names() |>
   dplyr::select(group:age) |>
-  dplyr::mutate(gender = factor(gender, labels = c("M", "F")))
+  dplyr::mutate(
+    gender = factor(gender, labels = c("M", "F")),
+    date = stringr::str_c(
+      sample(28, nrow(medicaldata::smartpill), replace = TRUE),
+      sample(12, nrow(medicaldata::smartpill), replace = TRUE),
+      sample(1983:2023, nrow(medicaldata::smartpill), replace = TRUE),
+      sep = "-"
+    ) |>
+      lubridate::dmy()
+  )
 
 conditions_list <- list(
-  gender = "group == '1'",
+  gender = "age <= 80",
   race = "",
   height = "",
   weight = "",
   age = ""
 )
 
-
-disc_var <- make_var_list(
+foo_vars <- make_var_list(
   data_frame,
   complete_list(conditions_list), "gender"
 )
-
-disc_var <- make_var_list(
+foo_vars <- make_var_list(
   data_frame,
   complete_list(conditions_list)
 )
 
-x <- disc_var[[6]][[1]]
-x[c(85, 78, 45)] <- NA
+cont_var <- foo_vars[[6]]
+cont_var[, 2] <- factor(cont_var[, 2], levels = c("F", "M"))
+cont_var[sample(87, 5), 2] <- NA
+cont_var[sample(87, 15), 1] <- NA
+summarise_continous_var(cont_var, use_NA = "ifany")
 
+disc_var <- foo_vars[[3]]
+disc_var[, 2] <- factor(disc_var[, 2], levels = c("F", "M"))
+disc_var[sample(87, 15), 2] <- NA
+disc_var[sample(87, 15), 1] <- NA
+summarise_discrete_var(disc_var)
 
-cont_var <-  make_var_list(
-  data_frame[, 4:6]
-)[[1]]
+date_var <- foo_vars[[7]]
+date_var[, 2] <- factor(date_var[, 2], levels = c("F", "M"))
+date_var[sample(87, 5), 2] <- NA
+date_var[sample(87, 15), 1] <- NA
+summarise_continous_var(date_var, use_NA = "no")
 
-summary_tibble(cont_var[,1])
-summary(cont_var[,1])
+medicaldata::smartpill |>
+  janitor::clean_names() |>
+  dplyr::select(group:age)
+
+data_frame |>
+  dplyr::mutate(
+    race = factor(race)
+  ) |>
+  summarise_df()
+
 

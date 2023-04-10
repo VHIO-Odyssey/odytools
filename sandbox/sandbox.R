24350e80dd4b20228aa069f68cf77115d774d823
garcia-galea <eduardogarcia@vhio.net>
2023-04-10 16:32:52 +0200

    Rename descriptive_tables to data_summary

--- sandbox/sandbox.R
+++ sandbox/sandbox.R
@@ -15,49 +15,21 @@ data_frame <- medicaldata::smartpill |>
 data_frame$gender[sample(95, 15)] <- NA
 data_frame$race[sample(95, 15)] <- NA
 
-(result <- data_frame |>
+data_frame |>
   dplyr::mutate(
     group = factor(group),
     race = factor(race)
   ) |>
   ody_summarise_df(
     grouping_var = "gender",
-    conditions_list = list(
-      race = "group == 1",
-      height = "",
-      weight = "",
-      age = "",
-      date = ""
-    ),
     compare_groups = TRUE,
     exclude = "group",
-    use_NA = "ifany"))
-
-result$age$mean
-
-
-comparison <- compareGroups::compareGroups(
-  gender ~ height + age + weight + group, data_frame, na.action = "na.exclude"
-)
-
-compareGroups::createTable(comparison)
-
-data_frame |>
-  dplyr::select(gender, height, age, weight, group) |>
-  make_var_list(grouping_var = "gender") |>
-  purrr::map(
-    function(data_frame) {
-      if (ncol(data_frame) == 1) return("No  groups")
-      form <- call(
-        "as.formula",
-        stringr::str_c(
-          colnames(data_frame)[2], " ~ ",
-          colnames(data_frame)[1]
-        )
-      )
-
-      compareGroups::compareGroups(eval(form), data_frame)
-    }
+    use_NA = "ifany"
   )
 
 
+data_frame |>
+  dplyr::group_by(gender) |>
+  dplyr::summarise(
+    altura = mean(height)
+  )

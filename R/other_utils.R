95bd6d6971d49bd380871adc6ded93523f4800c6
garcia-galea <eduardogarcia@vhio.net>
2023-07-04 16:36:00 +0200

    Add ody_change_names

--- R/other_utils.R
+++ R/other_utils.R
@@ -39,3 +39,39 @@ ody_add_version <- function(file_name, extension = "html") {
   stringr::str_c(file_name, current_ver, ".", extension)
 
 }
+
+
+
+#' Change column names
+#'
+#' Modify column names according to specified changes in a variables data frame.
+#'
+#' @param data_frame Data frame the names should by modified
+#' @param names_df 2 columns data frame. The first column must contain the new names and the second one, the current ones.
+#'
+#' @return data_frame with names changes according to names_df.
+#' @export
+ody_change_names <- function(data_frame, names_df) { #CANDIDATE
+
+  data_frame_names <- names(data_frame)
+
+  names(names_df) <- c("new", "current")
+
+  new_names <- purrr::map_chr(
+    data_frame_names,
+    function(x) {
+
+      name <- dplyr::filter(names_df, .data$current == x) |>
+        dplyr::pull("new")
+
+      if (length(name) == 0) x else name
+
+    }
+  )
+
+  names(data_frame) <- new_names
+
+  data_frame
+
+}
+

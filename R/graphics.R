e3d29ef271d205179e663ed11fde9d04cce7f2c7
garcia-galea <eduardogarcia@vhio.net>
2024-09-27 12:55:29 +0200

    Add data attribute to ody_plot_violindotbox

--- R/graphics.R
+++ R/graphics.R
@@ -13,8 +13,8 @@ NULL
 #' @param compare Logical indicating if a statistical comparison should be added.
 #' @param p_adj A string indicating the method to adjust the p-values. See \link[stats]{p.adjust}.
 #' @param brackets_pos A numeric value indicating the relative position of the brackets that represent the statistical comparison.
-#' @param ... Additional arguments to be passed to \link[ggpubr]{compare_means} (the function
-#' used to compare the means) and to \link[ggpubr]{geom_bracket} (the function used to add the brackets).
+#' @param ... Additional arguments to be passed to \link[ggpubr]{geom_bracket} (the function used to add the brackets).
+#' @details The data used for the plot is stored as an attribute of the returned ggplot object called `data`
 #' @return A ggplot object that can be further customized.
 #' @export
 ody_plot_violindotbox <- function(
@@ -38,7 +38,7 @@ ody_plot_violindotbox <- function(
     p <-
       ggplot2::ggplot(data, ggplot2::aes(.data[[x]], .data[[y]])) +
       ggplot2::geom_boxplot(outliers = FALSE) +
-      ggplot2::geom_jitter(alpha = 0.5)
+      ggplot2::geom_jitter(alpha = 0.5, width = 0.2)
 
   } else {
 
@@ -48,14 +48,14 @@ ody_plot_violindotbox <- function(
       ggplot2::geom_boxplot(width = 0.1, outliers = FALSE, fill = "gray") +
       gghalves::geom_half_point(side = "l", alpha = 0.5)
 
-
   }
+
   if (compare) {
 
     stats <-
       ggpubr::compare_means(
         formula(glue::glue("{y} ~ {x}")),
-        data = data, ...
+        data = data, p.adjust.method = p_adj
       )
 
     y_pos <- max(data |> dplyr::pull(.data[[y]]), na.rm = TRUE) * brackets_pos
@@ -68,11 +68,13 @@ ody_plot_violindotbox <- function(
           xmin = .data[["group1"]], xmax = .data[["group2"]],
           label = gtsummary::style_pvalue(.data[["p.adj"]])
         ),
-        y.position = y_pos, ...#step.increase = 0.07, label.size = 3,  tip.length = 0
+        y.position = y_pos, ...
       )
 
   }
 
+  data <- data |> dplyr::select(.data[[x]], .data[[y]])
+  attr(p, "data") <- data
   p
 
 }

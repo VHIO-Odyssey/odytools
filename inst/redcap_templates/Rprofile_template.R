6501647a74eaaf394233422b7ce47ed2f7c1f003
garcia-galea <eduardogarcia@vhio.net>
2025-11-14 10:01:26 +0100

    Use cli::cat_rule in Rprofile templates
    Replaces cli::rule with cli::cat_rule in both project and REDCap Rprofile templates for improved output formatting. Also removes unnecessary leading blank lines.

--- inst/redcap_templates/Rprofile_template.R
+++ inst/redcap_templates/Rprofile_template.R
@@ -1,4 +1,3 @@
-
 source(here::here(
   list.files(here::here(), "_dependencies.R$")
 ))
@@ -6,7 +5,7 @@ source(here::here(
 # Uncomment to set gander chat model
 # options(.gander_chat = ellmer::chat_openai(model = "gpt-4o-mini"))
 
-cli::rule(
+cli::cat_rule(
   left = "Odytools REDCap Project",
   right = utils::packageVersion("odytools"),
   line = 2
